42382ad39a00d857ce8357ac4f183f9555487999
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-01 17:30:05 -0400

    better logging

--- src/main/java/com/redcraft86/simplecloudscompat/mixin/MixinPlugin.java
+++ src/main/java/com/redcraft86/simplecloudscompat/mixin/MixinPlugin.java
@@ -4,22 +4,26 @@ import java.util.Set;
 import java.util.List;
 import java.util.HashSet;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 import org.objectweb.asm.tree.ClassNode;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 public class MixinPlugin implements IMixinConfigPlugin {
+    private static final Logger LOGGER = LogUtils.getLogger();
     private static final Set<String> loadedMods = new HashSet<>();
 
     @Override
     public boolean shouldApplyMixin(String targetClass, String mixinClass) {
-        System.out.println("Mixin: " + mixinClass);
         if (mixinClass.contains(".cyclic.")) {
+            LOGGER.info("[Simple Clouds Compat] Loaded Mixin: {}", mixinClass);
             return isModIncluded("cyclic");
         }
         if (mixinClass.contains(".supplementaries.")) {
+            LOGGER.info("[Simple Clouds Compat] Loaded Mixin: {}", mixinClass);
             return isModIncluded("supplementaries");
         }
         return true;
